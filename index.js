038926a0dc003bbedd283b58b15cc302a762e9a6
AISHA-PRO <55359117+AISHA-PRO@users.noreply.github.com>
2019-12-08 23:47:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your Circle class here
+// Add your Circle class here
+class Circle{
+  constructor(radius){
+    this.radius = radius; 
+  }
+  get diameter(){
+    return 2 *this.radius*2;
+  }
+   set diameter(radius){
+    this.radius= radius/2;
+  }
+  
+   get circumference(){
+    return 2 * Math.PI * this.area ;
+  }
+   set circumference(radius){
+    this.radius= radius/2;
+  }
+  
+   get circumference(){
+    return 2* Math.PI *  this.area ;
+  }
+   set circumference(radius){
+    this.radius = circle / Math.PI / 2
+  }
+  
+  get area(){
+    return  Math.PI * Math.pow(this.radius , 2) 
+
+  }
+
+  
+}
