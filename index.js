6427dc266e4501f5c0ace0f8bd2bc1edd5effc1b
AISHA-PRO <55359117+AISHA-PRO@users.noreply.github.com>
2019-12-09 11:22:28 +0300

    done

--- index.js
+++ index.js
@@ -5,28 +5,26 @@ class Circle{
     this.radius = radius; 
   }
   get diameter(){
-    return 2 *this.radius*2;
+    return 2 *this.radius;
   }
    set diameter(radius){
     this.radius= radius/2;
   }
   
-   get circumference(){
-    return 2 * Math.PI * this.area ;
-  }
    set circumference(radius){
-    this.radius= radius/2;
+    this.radius= radius/(Math.PI * 2);
   }
   
    get circumference(){
     return 2* Math.PI *  this.area ;
   }
-   set circumference(radius){
-    this.radius = circle / Math.PI / 2
-  }
   
   get area(){
     return  Math.PI * Math.pow(this.radius , 2) 
+
+  }
+  set area(radius){
+    this.radius= radius;
 
   }
 
